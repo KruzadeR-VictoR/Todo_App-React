27244fc76872ccb43f3502b42db929807400e45e
KruzadeR-VictoR <victorbiju31@gmail.com>
2022-07-31 17:16:41 +0530

    updated the todos functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,146 +1,110 @@
 import { useEffect, useState } from "react";
+import closeBtn from "./assets/close.svg";
+import close from "./assets/cardClose.svg";
 import "./App.css";
 
 function App() {
   const [IsAdd, setIsAdd] = useState(false);
-  const [Id, setId] = useState(0);
 
-  const [Todo, setTodo] = useState([]);
+  const [Todo, setTodo] = useState([
+    {
+      title: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+      suscipit!`,
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+      officiis totam dolores quia earum facere voluptatibus nihil
+      perspiciatis nulla expedita?`,
+    },
+    {
+      title: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+      suscipit!`,
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+      officiis totam dolores quia earum facere voluptatibus nihil
+      perspiciatis nulla expedita?`,
+    },
+  ]);
   const [Title, setTitle] = useState();
   const [Description, setDescription] = useState();
-  const [Date, setDate] = useState();
 
+  // toggles the todo form
   const handleForm = () => {
     if (IsAdd === false) setIsAdd((prev) => !prev);
-    else return;
   };
 
-  const handleCreateTodo = (e) => {
-    e.preventDefault();
-    console.log(Date().toLocaleString());
+  // toggles the todo form
+  const handleClose = () => {
     if (IsAdd === true) {
       setIsAdd(false);
     }
-    setTodo({
-      id: `${Id}`,
-      title: `${Title}`,
-      description: `${Description}`,
-      date: `${Date}`,
-    });
   };
 
-  // get the current date
+  // close the todo card
+  const handleCardClose = (task) => {
+    console.log(task);
+    setTodo(
+      Todo.filter((item) => {
+        return item !== task;
+      })
+    );
+  };
+  // handles form data on submit
+  const handleCreateTodo = (e) => {
+    if (Title == "" || Description == "") {
+      alert("Please fill the Todo Form");
+    } else {
+      e.preventDefault();
+      if (IsAdd === true) {
+        setIsAdd(false);
+      }
 
-  const dateNow = new Date();
-  // const Current=dateNow
-  console.log(dateNow);
+      setTodo([
+        ...Todo,
+        {
+          title: Title,
+          description: Description,
+        },
+      ]);
+    }
+  };
 
   useEffect(() => {
-    console.log(Todo);
-    setId(Id + 1);
-    // setTitle("");
-    // setDescription("");
-    // setDate("");
+    setTitle("");
+    setDescription("");
   }, [Todo]);
 
   return (
     <>
-      <section className="App">
+      <section className={IsAdd ? "App overlay" : "App"}>
         {/* logo  */}
         <div className="logo">
           <h1>Todo App</h1>
         </div>
         {/* Add a Todo  */}
         <button className="addTodo" onClick={handleForm}>
-          {" "}
           + Todo
         </button>
         {/* My Todos  */}
         <div className="All-Todos">
           <h1>My Todos</h1>
           <div className="Todos">
-            <div className="Todo-card">
-              <h1 className="heading">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                omnis?
-              </h1>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate rerum eaque exercitationem officiis minus voluptates
-                distinctio est, vitae maxime explicabo odit voluptatum quisquam
-                quam? Voluptas!
-              </p>
-              <div className="created">
-                <span>Created at : </span>
-                <span className="time"></span>
-              </div>
-            </div>
-            <div className="Todo-card">
-              <h1 className="heading">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                omnis?
-              </h1>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate rerum eaque exercitationem officiis minus voluptates
-                distinctio est, vitae maxime explicabo odit voluptatum quisquam
-                quam? Voluptas!
-              </p>
-              <div className="created">
-                <span>Created at : </span>
-                <span className="time"></span>
-              </div>
-            </div>
-            <div className="Todo-card">
-              <h1 className="heading">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                omnis?
-              </h1>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate rerum eaque exercitationem officiis minus voluptates
-                distinctio est, vitae maxime explicabo odit voluptatum quisquam
-                quam? Voluptas!
-              </p>
-              <div className="created">
-                <span>Created at : </span>
-                <span className="time"></span>
-              </div>
-            </div>
-            <div className="Todo-card">
-              <h1 className="heading">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                omnis?
-              </h1>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate rerum eaque exercitationem officiis minus voluptates
-                distinctio est, vitae maxime explicabo odit voluptatum quisquam
-                quam? Voluptas!
-              </p>
-              <div className="created">
-                <span>Created at : </span>
-                <span className="time"></span>
-              </div>
-            </div>
-            <div className="Todo-card">
-              <h1 className="heading">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                omnis?
-              </h1>
-              <p className="description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate rerum eaque exercitationem officiis minus voluptates
-                distinctio est, vitae maxime explicabo odit voluptatum quisquam
-                quam? Voluptas!
-              </p>
-              <div className="created">
-                <span>Created at : </span>
-                <span className="time"></span>
-              </div>
-            </div>
+            {Todo.length === 0 ? (
+              <h1 className="noTodos">No Todos available</h1>
+            ) : (
+              Todo?.map((task, index) => (
+                <div className="Todo-card" key={index}>
+                  <h1 className="heading">{task.title}</h1>
+                  <p className="description">{task.description}</p>
+                  <button
+                    className="close-card"
+                    onClick={() => handleCardClose(task)}
+                  >
+                    <img src={close} alt="close card" />
+                  </button>
+                </div>
+              ))
+            )}
           </div>
         </div>
+        {/* <h1>{Todo}</h1> */}
         {/* add Todo Form  */}
         <form className={IsAdd ? "Todo-Form activeForm" : "Todo-Form"}>
           <h1 className="create-heading">Create a new Todo</h1>
@@ -149,6 +113,7 @@ function App() {
             type="text"
             name="title"
             id="heading"
+            value={IsAdd ? Title : ""}
             placeholder="Title / heading"
             required
             onChange={(e) => setTitle(e.target.value)}
@@ -160,24 +125,15 @@ function App() {
             cols="30"
             rows="10"
             maxLength="100%"
+            value={IsAdd ? Description : ""}
             placeholder="Description for your Todo"
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
-          <label htmlFor="createdTime" className="createdFor">
-            <span> Remind me on</span>
-            <div>
-              <input
-                type="datetime-local"
-                name=""
-                id="time"
-                value={Date ? Date : ""}
-                onChange={(e) => setDate(e.target.value)}
-              />
-              <span> ( optional )</span>
-            </div>
-          </label>
           <button type="Submit" className="add" onClick={handleCreateTodo}>
             ADD
+          </button>
+          <button className="close" onClick={handleClose}>
+            <img src={closeBtn} alt="Close Button" />
           </button>
         </form>
       </section>
